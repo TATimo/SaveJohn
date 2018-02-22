62f8c8394b2f9a02d0d1672f28c6c4cbe20efe8b
Yiyuan Chen <ianchen227@gmail.com>
2018-02-21 21:46:58 -0800

    add fire script and collider, change laser color

---
+++ Assets/Scripts/Fire/FireAttack.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireAttack : MonoBehaviour
+{
+	public float timeBetweenAttacks = 1.0f;
+	public int attackDamage = 5;
+	public float waitAttack = 0.3f;
+
+	Animator anim;
+	GameObject player;
+	PlayerHealth playerHealth;
+	bool playerInRange;
+	float timer;
+
+
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+		anim = GetComponent <Animator> ();
+	}
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = true;
+		}
+	}
+
+
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = false;
+		}
+	}
+
+
+	void Update ()
+	{
+		timer += Time.deltaTime;
+
+		if(timer >= timeBetweenAttacks && playerInRange)
+		{
+			Attack ();
+		}
+
+		if(playerHealth.currentHealth <= 0)
+		{
+			anim.SetTrigger ("PlayerDead");
+		}
+	}
+
+
+	void Attack ()
+	{
+		timer = 0f;
+
+		if(playerHealth.currentHealth > 0)
+		{
+			playerHealth.TakeDamage (attackDamage);
+			//			Invoke ("SetAttackFlag", waitAttack);
+		}
+	}
+
+}
